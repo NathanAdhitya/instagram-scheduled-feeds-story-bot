2ccff30a83354a198f7c5b0a2f69b4600959bd4f
NathanAdhitya <me@natha.my.id>
2023-03-09 20:24:45 +0700

    make gotify optional

--- utils/gotify_util.py
+++ utils/gotify_util.py
@@ -4,15 +4,20 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-gotify = Gotify(
-    base_url=os.environ.get("GOTIFY_BASE_URL"),
-    app_token=os.environ.get("GOTIFY_TOKEN"),
-)
+gotify = None
+
+if os.environ.get("GOTIFY_BASE_URL") is not None:
+    gotify = Gotify(
+        base_url=os.environ.get("GOTIFY_BASE_URL"),
+        app_token=os.environ.get("GOTIFY_TOKEN"),
+    )
 
 
 def send_notification(message: str, title: str):
-    gotify.create_message(
-        message,
-        title=title,
-        priority=5,
-    )
+    # Do nothing if gotify is not specified
+    if gotify is not None:
+        gotify.create_message(
+            message,
+            title=title,
+            priority=5,
+        )
